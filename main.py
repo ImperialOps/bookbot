217c03fedde0c62c0dfd61ad4c029222cb243e6a
Josh Bowen <joshb9398@gmail.com>
2025-04-03 10:12:34 +0100

    Pass the book file path as an argument

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_char_count, sort_count_descending
 
 def get_book_text(path: str):
@@ -6,8 +7,16 @@ def get_book_text(path: str):
         return content
 
 def main():
-    file_path = 'books/frankenstein.txt'
-    content = get_book_text(file_path)
+    args = sys.argv
+    if len(args) != 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+    file_path = args[1]
+    try:
+        content = get_book_text(file_path)
+    except Exception as e:
+        print(f'Unable to open file {file_path}: {str(e)}')
+        return
     num_words = get_num_words(content)
     char_count = get_char_count(content)
     sorted_count = sort_count_descending(char_count)
